e2e11be190d7e3c8d82e5891979cf5c09748873f
ishitashinghari <ishitashinghari06@gmail.com>
2026-04-22 19:38:35 +0530

    Add files via upload

---
+++ ClueEntry.java
@@ -0,0 +1,14 @@
+package crossword;
+
+public class ClueEntry {
+    public int number;
+    public String word;
+    public String clue;
+    public String hint;
+    public int row;
+    public int col;
+    public char dir;
+    public int length;
+
+    public ClueEntry() {}
+}
